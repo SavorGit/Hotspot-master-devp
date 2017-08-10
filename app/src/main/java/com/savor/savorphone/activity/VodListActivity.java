e231a658ed897851f5b892960c03d70f6ee1ed94
hezd <hezd0924@163.com>
2017-08-10 17:56:00 +0800

    c

--- app/src/main/java/com/savor/savorphone/activity/VodListActivity.java
+++ app/src/main/java/com/savor/savorphone/activity/VodListActivity.java
@@ -1,15 +1,18 @@
 package com.savor.savorphone.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.common.api.widget.pulltorefresh.library.PullToRefreshListView;
 import com.savor.savorphone.R;
 import com.savor.savorphone.adapter.VodListAdapter;
 
-public class VodListActivity extends BaseProActivity {
+public class VodListActivity extends BaseProActivity implements View.OnClickListener {
 
     private PullToRefreshListView mVodListView;
     private VodListAdapter mVodAdapter;
+    private ImageView mBackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +26,26 @@ public class VodListActivity extends BaseProActivity {
 
     public void getViews() {
         mVodListView = (PullToRefreshListView) findViewById(R.id.ptl_vod_list);
-
+        mBackBtn = (ImageView) findViewById(R.id.iv_left);
     }
 
     @Override
     public void setViews() {
         mVodAdapter = new VodListAdapter(this);
         mVodListView.setAdapter(mVodAdapter);
+    }
+
+    @Override
+    public void setListeners() {
+        mBackBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_left:
+                finish();
+                break;
+        }
     }
 }
